80cdb4388e771eef1c10523ee53f9ddecca27cd1
Zachary Lumpkins <zdoc01@gmail.com>
2017-10-14 12:46:48 -0700

    Read port from env var for prod deployment

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 const app = express();
+const ENV = process.env;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
@@ -11,6 +13,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(3000, function() {
-  console.log('Server listening on port 3000');
+app.listen(PORT, function() {
+  console.log(`Server listening on port ${PORT}`);
 });
