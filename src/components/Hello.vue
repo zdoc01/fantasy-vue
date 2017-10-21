1e6b4304777f8c1722a5b20eebd20d81dc0d509f
Zachary Lumpkins <zdoc01@gmail.com>
2017-10-21 10:47:04 -0700

    Add link to week 4 surivor stats

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -7,6 +7,7 @@
       <li><a href="/api/stats/week/1">Week 1</a></li>
       <li><a href="/api/stats/week/2">Week 2</a></li>
       <li><a href="/api/stats/week/3">Week 3</a></li>
+      <li><a href="/api/stats/week/4">Week 4</a></li>
     </ul>
   </div>
 </template>
