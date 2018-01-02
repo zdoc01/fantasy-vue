3576b95c1ea891bd733a3c8af8be211af0b3a2a7
Zachary Lumpkins <zdoc01@gmail.com>
2018-01-01 22:40:30 -0800

    Enable CORS for external API requests

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 const ENV = process.env;
 const PORT = ENV.PORT || 3000;
+
+// enable cross-origin requests to the API
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
